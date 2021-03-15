64012e21873dbe31fd166b7b171619fec294d4cd
rahuldubey99 <rd995628@gmail.com>
2021-03-15 18:54:18 +0530

    first commit

--- plandjango/settings.py
+++ plandjango/settings.py
@@ -124,8 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
     "https://guarded-island-41833.herokuapp.com",
     "http://guarded-island-41833.herokuapp.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    
 ]
