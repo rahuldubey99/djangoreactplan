711bba7c675e2284b6e03057579255f0f57650fc
rahuldubey99 <rd995628@gmail.com>
2021-03-15 14:22:05 +0530

    any comment

---
+++ api/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Plan
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = ['id', 'item']
---
+++ api/views.py
@@ -0,0 +1,17 @@
+from .models import Plan
+from rest_framework.generics import ListAPIView, CreateAPIView,DestroyAPIView
+from .serializers import PlanSerializer
+
+# Create your views here.
+class PlanList(ListAPIView):
+    queryset = Plan.objects.all()
+    serializer_class = PlanSerializer
+
+class PlanCreate(CreateAPIView):
+    queryset = Plan.objects.all()
+    serializer_class = PlanSerializer
+
+class PlanDestroy(DestroyAPIView):
+    queryset = Plan.objects.all()
+    serializer_class = PlanSerializer
+    lookup_field = 'id'
